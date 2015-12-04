51fa3edddb70b5aad0d7aec1fb82ac3caea4e623
Kevin Retzke <retzkek@gmail.com>
2015-12-04 03:15:13 -0600

    split proxy into own file

--- main.go
+++ main.go
@@ -6,49 +6,10 @@ import (
 	"crypto/x509"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"os"
 )
 
 var authorizedCNs = []string{"Kevin Retzke 3130"}
-
-func authorizedCN(certs []*x509.Certificate) (bool, string) {
-	for _, cert := range certs {
-		for _, cn := range authorizedCNs {
-			if cert.Subject.CommonName == cn {
-				return true, cn
-			}
-		}
-	}
-	return false, ""
-}
-
-type ProxyServer struct {
-	Address string
-	Port    string
-
-	proxy *httputil.ReverseProxy
-}
-
-func NewProxyServer(address, port, target_url string) *ProxyServer {
-	t, err := url.Parse(target_url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &ProxyServer{Address: address,
-		Port:  port,
-		proxy: httputil.NewSingleHostReverseProxy(t),
-	}
-}
-
-func (p *ProxyServer) handler(w http.ResponseWriter, req *http.Request) {
-	if auth, _ := authorizedCN(req.TLS.PeerCertificates); auth {
-		p.proxy.ServeHTTP(w, req)
-	} else {
-		http.Error(w, "not authorized", http.StatusUnauthorized)
-	}
-}
 
 func main() {
 	proxy := NewProxyServer("localhost", "8443", "http://localhost:9200")
