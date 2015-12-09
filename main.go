175dde8d54cdf4762c9d9c82a54b2a013e9bc47e
Kevin Retzke <retzkek@gmail.com>
2015-12-08 19:57:56 -0600

    use gorilla/mux for routing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
@@ -12,8 +13,13 @@ import (
 var authorizedCNs = []string{"Kevin Retzke 3130"}
 
 func main() {
+	r := mux.NewRouter()
+
+	s := r.PathPrefix("/es").Subrouter()
+
 	proxy := NewProxyServer("localhost", "8443", "http://localhost:9200")
-	http.Handle("/", proxy)
+	s.Handle("/", proxy)
+
 	log.Printf("About to listen on 8443. Go to https://127.0.0.1:8443/")
 	srv := http.Server{Addr: ":8443"}
 
@@ -30,6 +36,7 @@ func main() {
 	if ok := pool.AppendCertsFromPEM(buf.Bytes()); !ok {
 		log.Fatal(err)
 	}
+	http.Handle("/", r)
 
 	srv.TLSConfig = &tls.Config{ClientAuth: tls.VerifyClientCertIfGiven,
 		ClientCAs: pool}
@@ -37,4 +44,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
