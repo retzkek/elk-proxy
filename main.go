7f1aaa25761b873c148ba327a666a160e11134a1
Kevin Retzke <kretzke@fnal.gov>
2016-02-10 18:14:03 -0600

    working auth

--- main.go
+++ main.go
@@ -5,24 +5,15 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"regexp"
 )
-
-func authorizedCN(certs []*x509.Certificate, cns []string) (bool, string) {
-	for _, cert := range certs {
-		for _, cn := range cns {
-			if cert.Subject.CommonName == cn {
-				return true, cn
-			}
-		}
-	}
-	return false, ""
-}
 
 type ProxyServer struct {
 	Config *config
@@ -40,12 +31,90 @@ func NewProxyServer(config *config) *ProxyServer {
 	return &p
 }
 
+func (p *ProxyServer) authorize(req *http.Request, authType string, authCNs []string) (bool, string) {
+	switch authType {
+	case "any":
+		return true, "anonymous access"
+	case "any_cert":
+		if req.TLS != nil && len(req.TLS.PeerCertificates) > 0 {
+			return true, "user presented valid certificate"
+		}
+	case "cert":
+		for _, presCert := range req.TLS.PeerCertificates {
+			for _, cn := range authCNs {
+				if presCert.Subject.CommonName == cn {
+					return true, fmt.Sprintf("user presented certificate with authorized CommonName (%s)", cn)
+				}
+			}
+		}
+	}
+	return false, ""
+}
+
+func (p *ProxyServer) authorizeRequest(req *http.Request, role string) bool {
+	logger := log.WithFields(log.Fields{
+		"path":    req.URL.Path,
+		"method":  req.Method,
+		"address": req.RemoteAddr,
+	})
+	if authed, authReason := p.authorize(req, p.Config.Global.AdminAuth, p.Config.Global.AdminCerts); authed {
+		logger.Infof("authorized global admin: %s", authReason)
+		return true
+	}
+	if role == "write" {
+		if authed, authReason := p.authorize(req, p.Config.Global.WriteAuth, p.Config.Global.WriteCerts); authed {
+			logger.Infof("authorized global write: %s", authReason)
+			logger.Info(authReason)
+			return true
+		}
+	}
+	if role == "read" {
+		if authed, authReason := p.authorize(req, p.Config.Global.ReadAuth, p.Config.Global.ReadCerts); authed {
+			logger.Infof("authorized global read: %s", authReason)
+			return true
+		}
+	}
+
+	// index-specific auth
+	for _, index := range p.Config.Indexes {
+		matched, err := regexp.MatchString(index.IndexPattern, req.URL.Path)
+		if err != nil {
+			logger.Errorf("unauthorized due to error: %s", err)
+			return false
+		}
+		if matched {
+			if authed, authReason := p.authorize(req, index.AdminAuth, index.AdminCerts); authed {
+				logger.WithField("index", index.IndexPattern).Infof("authorized index admin: %s", authReason)
+				return true
+			}
+			if role == "write" {
+				if authed, authReason := p.authorize(req, index.WriteAuth, index.WriteCerts); authed {
+					logger.WithField("index", index.IndexPattern).Infof("authorized index write: %s", authReason)
+					return true
+				}
+			}
+			if role == "read" {
+				if authed, authReason := p.authorize(req, index.ReadAuth, index.ReadCerts); authed {
+					logger.WithField("index", index.IndexPattern).Infof("authorized index read: %s", authReason)
+					return true
+				}
+			}
+		}
+	}
+	logger.Warning("unauthorized")
+	return false
+}
+
 func (p *ProxyServer) ServeRead(w http.ResponseWriter, req *http.Request) {
-	p.Proxy.ServeHTTP(w, req)
+	if p.authorizeRequest(req, "read") {
+		p.Proxy.ServeHTTP(w, req)
+	} else {
+		http.Error(w, "not authorized", http.StatusUnauthorized)
+	}
 }
 
 func (p *ProxyServer) ServeWrite(w http.ResponseWriter, req *http.Request) {
-	if auth, _ := authorizedCN(req.TLS.PeerCertificates, p.Config.Global.WriteCerts); auth {
+	if p.authorizeRequest(req, "write") {
 		p.Proxy.ServeHTTP(w, req)
 	} else {
 		http.Error(w, "not authorized", http.StatusUnauthorized)
@@ -53,7 +122,7 @@ func (p *ProxyServer) ServeWrite(w http.ResponseWriter, req *http.Request) {
 }
 
 func (p *ProxyServer) ServeAdmin(w http.ResponseWriter, req *http.Request) {
-	if auth, _ := authorizedCN(req.TLS.PeerCertificates, p.Config.Global.AdminCerts); auth {
+	if p.authorizeRequest(req, "admin") {
 		p.Proxy.ServeHTTP(w, req)
 	} else {
 		http.Error(w, "not authorized", http.StatusUnauthorized)
@@ -77,10 +146,8 @@ func main() {
 
 	proxy := NewProxyServer(config)
 	// Admin
-	r.PathPrefix("/_cat").Methods("GET").HandlerFunc(proxy.ServeAdmin)
-	r.PathPrefix("/_stats").Methods("GET").HandlerFunc(proxy.ServeAdmin)
-	r.PathPrefix("/_plugin").Methods("GET").HandlerFunc(proxy.ServeAdmin)
-	r.PathPrefix("/_template").Methods("POST").HandlerFunc(proxy.ServeAdmin)
+	r.PathPrefix("/_").Methods("GET").HandlerFunc(proxy.ServeAdmin)
+	r.PathPrefix("/_").Methods("POST").HandlerFunc(proxy.ServeAdmin)
 	r.PathPrefix("/").Methods("DELETE").HandlerFunc(proxy.ServeAdmin)
 	// Write
 	r.PathPrefix("/").Methods("POST").HandlerFunc(proxy.ServeWrite)
